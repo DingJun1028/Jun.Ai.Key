1825fbe4216e0eb9db94421da76def99ac8e9000
DingJun1028 <DingJunHong1028@gmail.com>
2025-06-12 05:48:47 +0800

    完成合併與亮色主題設定，重訂專案基底。

---
+++ .history/webpack.config_20250612052825.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/template.html',
+    }),
+    new Dotenv({ systemvars: true }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  mode: 'development',
+};
